f0fb95b85da8f881600fdd3bde4c2e728216665c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-20 09:10:53 +0100

    Creazione del test variabili

---
+++ Collezione.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Collezione.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestVariabili()
+        {
+            int a = 2, b = a;
+            Assert.AreEqual(a, b);
+        }
+
+
+       
+    }
+}
